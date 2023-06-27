1183f64852ddc40df38ac44cb1c8b4d7b780e047
bit-web24 <bitweb24@gmail.com>
2023-06-28 00:07:31 +0530

    update color scheme

--- app/src/components/Todo.jsx
+++ app/src/components/Todo.jsx
@@ -9,22 +9,22 @@ const Todo = ({ title, content }) => {
   };
 
   return (
-    <div className={`bg-yellow-200 rounded-lg p-4 shadow-md flex flex-col ${completed ? "opacity-50" : ""}`}>
+    <div className={`bg-gray-100 rounded-lg p-4 shadow-md flex flex-col ${completed ? "opacity-50" : ""}`}>
       <div className="flex items-center justify-between mb-4">
-        <h2 className={`text-2xl font-bold ${completed ? "line-through text-gray-500" : "text-purple-700"}`}>{title}</h2>
+        <h2 className={`text-2xl font-bold ${completed ? "line-through text-gray-600" : "text-blue-800"}`}>{title}</h2>
         <div className="bg-red-500 rounded-full p-2 cursor-pointer">
           <RiDeleteBin2Line className="text-white cursor-pointer" size={20} />
         </div>
       </div>
-      <div className={`bg-gray-100 rounded-lg p-4 mb-4 ${completed ? "opacity-50" : ""}`}>
-        <p className={`text-lg font-semibold ${completed ? "text-gray-500" : "text-gray-800"}`}>{content}</p>
+      <div className={`bg-white rounded-lg p-4 mb-4 ${completed ? "opacity-50" : ""}`}>
+        <p className={`text-lg font-semibold ${completed ? "text-gray-600" : "text-gray-800"}`}>{content}</p>
       </div>
       <button
-        className={`bg-green-400 text-red font-semibold py-2 px-4 rounded-full focus:outline-none ${
+        className={`bg-green-500 text-lg text-white font-semibold py-2 px-4 rounded-full focus:outline-none ${
           completed ? "hidden" : ""
         }`}
         onClick={handleCheck}
-        style={{fontFamily: 'Shikamaru'}}
+        style={{ fontFamily: "revert" }}
       >
         Mark Completed
       </button>
